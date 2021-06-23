2324e4281286d8036bcc3af38d3364214cb33b1a
citharus <citharus@protonmail.com>
2021-06-23 22:10:17 +0200

    :art: Changed double quote to single quote

--- config/types.py
+++ config/types.py
@@ -1,7 +1,7 @@
 import re
 
-__all__: list[str] = ["INTEGER", "FLOAT", "LIST"]
+__all__: list[str] = ['INTEGER', 'FLOAT', 'LIST']
 
-INTEGER: re.Pattern = re.compile(r"\d+")
-FLOAT: re.Pattern = re.compile(r"\d+\.\d+")
-LIST: re.Pattern = re.compile(r"\[.*?\]")
+INTEGER: re.Pattern = re.compile(r'\d+')
+FLOAT: re.Pattern = re.compile(r'\d+\.\d+')
+LIST: re.Pattern = re.compile(r'\[.*?\]')
