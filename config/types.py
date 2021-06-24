e88c0f2306f92b8134b305fd62509db3144e0a01
citharus <citharus@protonmail.com>
2021-06-24 17:32:07 +0200

    :recycle: Changed types to classes

--- config/types.py
+++ config/types.py
@@ -1,7 +1,28 @@
 import re
+from typing import AnyStr, Union
 
-__all__: list[str] = ['INTEGER', 'FLOAT', 'LIST']
+__all__: list[str] = ['INT', 'FLOAT', 'LIST']
 
-INTEGER: re.Pattern = re.compile(r'\d+')
-FLOAT: re.Pattern = re.compile(r'\d+\.\d+')
-LIST: re.Pattern = re.compile(r'\[.*?\]')
+
+class TYPE:
+    def __init__(
+            self,
+            pattern: AnyStr,
+            flags: Union[int, re.RegexFlag] = 0,
+    ) -> None:
+        self.pattern: re.Pattern = re.compile(pattern, flags)
+
+
+class INT(TYPE):
+    def __init__(self) -> None:
+        super(INT, self).__init__(r'\d+')
+
+
+class FLOAT(TYPE):
+    def __init__(self) -> None:
+        super(FLOAT, self).__init__(r'\d+\.\d+')
+
+
+class LIST(TYPE):
+    def __init__(self) -> None:
+        super(LIST, self).__init__(r'\[.*?\]')
