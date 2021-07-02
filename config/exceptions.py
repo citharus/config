892cb5e86f6e594fb818120107b1e8d7d8c374ea
citharus <citharus@protonmail.com>
2021-07-02 21:55:45 +0200

    :sparkles: Added __all__

--- config/exceptions.py
+++ config/exceptions.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+__all__: list[str] = ['NoFileException']
+
 
 class NoFileException(Exception):
     def __init__(self) -> None:
